aaf14298c586ee7db18d5f39ae2fa485083b242c
Abubakr_Sokarno <abubakr.sokarno@ivas.com.eg>
2017-11-08 14:09:34 +0200

    abubakr, inhancements on image resizing

--- resources/views/dashboard/upload_resize.blade.php
+++ resources/views/dashboard/upload_resize.blade.php
@@ -22,6 +22,9 @@
                       <img id="image" src="#" class="main_resizing"/>
                     </div><br>
                     <input id="image_file" type='file' onchange="readURL(this);" />
+                    <br>
+                    <input id="inpWidth" placeholder="Width" style="display:none;"><br><br>
+                    <input id="inpHeight" placeholder="Height" style="display:none;">
                     <br><br>
                     <div class="form-group" style="display: none;" id="save_btn"> 
                         <input type="submit" class="btn btn-primary" value="@lang('messages.save')" onclick="save_image()">
@@ -37,39 +40,84 @@
 
 @section('script')
    <script>
-    var width = 250 ; 
-    var height = 300 ; 
+    var width = "250px" ; 
+    var height = "300px" ; 
+    var checker = false ; 
     $('#resizable').on('resize',function(){
-        width = this.style.width  ; 
-        height = this.style.height  ;
+        if(this.style.width || this.style.height)
+        {
+            width = this.style.width  ; 
+            height = this.style.height  ; 
+            if(!checker)
+                assing_w_h() ;   
+            checker = false ; 
+        }
     });
+       
     function readURL(input) {
            if (input.files && input.files[0]) {
                var reader = new FileReader();
-
                reader.onload = function (e) {
-                   $('#image')
+                    $('#image')
                        .attr('src', e.target.result)
                        .width(250)
                        .height(300);
-                   $('#resizable').css({
-                                        'width':250,
-                                        'height':300,
+                    $('#resizable').css({
+                                        'minWidth':250,
+                                        'minHeight':300,
                                         'display':'block'
                                         }) ;  
-                   $('#save_btn').css('display','block');
+                    $('#save_btn').css('display','block');
+                    $('#inpWidth').css('display','block') ; 
+                    $('#inpHeight').css('display','block') ; 
                };
                reader.readAsDataURL(input.files[0]);
-            }
+            }  
+        assing_w_h() ;
     }
+    
+    $('#inpWidth').keyup(function(){
+        width = parseInt(this.value) ;    
+        $('#image').width(width) ; 
+        $('#resizable').width(width) ;
+        checker = true ; 
+    }); 
+    $('#inpHeight').keyup(function(){
+        height = parseInt(this.value) ;    
+        $('#image').height(height) ;
+        $('#resizable').height(height) ;
+        checker = true ; 
+    }); 
+    function assing_w_h()
+    { 
+        if(parseInt(width) >= 250)
+        {
+            $('#inpWidth').val(width) ;
+        }
+        else
+        {
+            $('#inpWidth').val("250px") ;
+            width = "250px" ; 
+        }
+        if(parseInt(height) >= 300)
+        {
+            $('#inpHeight').val(height) ;
+        }
+        else
+        {
+            $('#inpHeight').val("300px") ;   
+            height = "300px"; 
+        }
+    }
+    
     function save_image()
     { 
         var theImage = $('#image_file').prop('files')[0] ; 
         var form_data = new FormData();
-        form_data.append('width',width); 
-        form_data.append('height',height) ;
-        form_data.append('image',theImage)
-       $.ajax({
+        form_data.append('width',parseInt(width)); 
+        form_data.append('height',parseInt(height)) ;
+        form_data.append('image',theImage) ;
+        $.ajax({
             type:"POST" , 
             url : "save_image", 
             data:  form_data , 
@@ -87,7 +135,7 @@
                 }
                 location.reload() ;
             }
-       });
+        });
     }
     </script>
     <script>

6881909ce0fba97d1927760d8226977e46b3c002
Abubakr_Sokarno <abubakr.sokarno@ivas.com.eg>
2017-11-16 16:40:52 +0200

    abubakr,fixing preview items in el finder

--- elFinder/php/connector.minimal.php
+++ elFinder/php/connector.minimal.php
@@ -99,7 +99,7 @@ $opts = array(
 		array(
 			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
 			'path'          => '../../uploads/',             // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../../uploads/', // URL to files (REQUIRED)
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload

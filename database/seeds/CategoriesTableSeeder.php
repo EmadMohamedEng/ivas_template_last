de6e29597af54a8459540acef6c08e86ee4da384
mohamed mahmoud <m.mahmoud@iVAS.local>
2019-02-20 09:49:55 +0200

    fixed migarte 20-2-2019

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -16,6 +16,24 @@ class CategoriesTableSeeder extends Seeder
 
         \DB::table('categories')->delete();
         
+        \DB::table('categories')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'title' => 'Aflam',
+                'image' => '1550152145324.jpg',
+                'created_at' => '2019-02-14 13:49:05',
+                'updated_at' => '2019-02-14 13:49:05',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'title' => 'Category 2',
+                'image' => '1550577156831.jpg',
+                'created_at' => '2019-02-14 14:35:00',
+                'updated_at' => '2019-02-19 11:52:36',
+            ),
+        ));
         
         
     }
--- database/seeds/ContentsTableSeeder.php
+++ database/seeds/ContentsTableSeeder.php
@@ -16,6 +16,41 @@ class ContentsTableSeeder extends Seeder
 
         \DB::table('contents')->delete();
         
+        \DB::table('contents')->insert(array (
+            0 => 
+            array (
+                'id' => 11,
+                'title' => 'Content 1',
+                'path' => '1550588179859.jpg',
+                'image_preview' => NULL,
+                'content_type_id' => 3,
+                'category_id' => 1,
+                'created_at' => '2019-02-19 09:14:20',
+                'updated_at' => '2019-02-19 14:56:19',
+            ),
+            1 => 
+            array (
+                'id' => 12,
+                'title' => 'dd',
+                'path' => '1550589274464.png',
+                'image_preview' => NULL,
+                'content_type_id' => 3,
+                'category_id' => 1,
+                'created_at' => '2019-02-19 09:14:38',
+                'updated_at' => '2019-02-19 15:14:34',
+            ),
+            2 => 
+            array (
+                'id' => 13,
+                'title' => 'f',
+                'path' => '1550590462462.mp4',
+                'image_preview' => '1550589300614.png',
+                'content_type_id' => 5,
+                'category_id' => 1,
+                'created_at' => '2019-02-19 09:15:20',
+                'updated_at' => '2019-02-19 15:34:22',
+            ),
+        ));
         
         
     }
